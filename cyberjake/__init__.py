8bb9aa5a96dc555c24f1679cdf49a047f56cfd12
Jacob White <jake@jwhite.network>
2021-06-23 12:04:41 -0400

    Function to check for newer version

--- cyberjake/__init__.py
+++ cyberjake/__init__.py
@@ -2,7 +2,8 @@
 from .discord_common import make_embed, error_embed
 from .database import build_database_url
 from .make_logger import make_logger
+from .check_update import check_update
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
-__all__ = ["__version__", "make_logger", "make_embed", "error_embed", "build_database_url"]
+__all__ = ["__version__", "make_logger", "make_embed", "error_embed", "build_database_url", "check_update"]
