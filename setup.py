f1b0dfd7c5a28ec117ccecdea17c5c014b70d203
Cyb3r-Jak3 <git@Cyberjake.xyz>
2023-01-28 15:00:31 -0500

    Fix classifier

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
-            "Programming Language :: Python :: Implementation :: PyP",
+            "Programming Language :: Python :: Implementation :: PyPy",
             "Programming Language :: Python :: Implementation :: CPython"
         ],
 )
